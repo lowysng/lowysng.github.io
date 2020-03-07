9dd718eb87690d18bdbc60da733b33f8ad5c2820
lowysng <44280469+lowysng@users.noreply.github.com>
2020-03-07 15:25:00 +0800

    Add v1 connect section and footer section

--- v1/script.js
+++ v1/script.js
@@ -3,13 +3,13 @@ document.addEventListener('scroll', () => {
     let nav = document.getElementById('nav')
 
     if (window.scrollY !== 0) {
-        nav.style.borderBottom = '2px solid #1f1f1f';
-        // nav.style.backgroundColor = '#1f1f1f';
+        nav.style.borderBottom = '2px solid lightgray';
+        nav.style.backgroundColor = '#fff';
 
 
     } else {
         nav.style.borderBottom = '2px solid transparent';
-        // nav.style.backgroundColor = '#fff';
+        nav.style.backgroundColor = '#fafafa';
     }
 
 })
