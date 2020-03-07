43ceb8066e0bc60c539bcca0089d326aba520fa3
lowysng <44280469+lowysng@users.noreply.github.com>
2020-03-07 22:51:18 +0800

    Fix navbar

--- v2/script.js
+++ v2/script.js
@@ -1,18 +1,15 @@
 document.addEventListener('scroll', () => {
 
     let nav = document.getElementById('nav')
-    let jump = document.getElementById('jump')
 
     if (window.scrollY !== 0) {
         nav.style.borderBottom = '2px solid lightgray';
         nav.style.backgroundColor = '#fff';
-        jump.style.color = '#2b2b2b'
 
 
     } else {
         nav.style.borderBottom = '2px solid transparent';
         nav.style.backgroundColor = '#fafafa';
-        jump.style.color = 'transparent'
 
     }
 
